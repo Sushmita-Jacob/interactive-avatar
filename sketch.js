25a9fe5731044240b259877cc3f394261e5ac4f1
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-07-14 23:23:33 +0000

    Added face shapes and color

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+let eyeWidth = 50;
+let eyeHeight = 40;
+let pupilWidth = 22;
+let pupilHeight = 25;
+
 function setup() {
     createCanvas(400, 400);
 }
@@ -5,9 +10,11 @@ function setup() {
 function draw() {
     rectMode(CENTER);
     angleMode(DEGREES);
-    background(255, 249, 226);
+    background(155, 249, 226);
     textSize(24);
-    textAlign(CENTER, CENTER);
+
+    // Abstract Art
+    /* textAlign(CENTER, CENTER);
     text("Brave, not perfect", width / 2, height / 2);
     ellipse(60, 320, 100, 150);
     line(200, 400, 500, 400);
@@ -16,5 +23,32 @@ function draw() {
     textSize(30);
     text("Sushmita", 300, 390);
     textSize(15);
-    text("This is a piece of art\nwe created to practice\ndrawing shapes and\ntext to a p5.js sketch.", 120, 75)
+    text("This is a piece of art\nwe created to practice\ndrawing shapes and\ntext to a p5.js sketch.", 120, 75) */
+
+    // Face
+    fill("BurlyWood");
+    ellipse(width / 2, height / 2, 175, 200);
+
+    // Eyes
+    fill("brown");
+    stroke("white");
+    strokeWeight(5);
+    ellipse(170, 170, eyeWidth, eyeHeight);
+    ellipse(230, 170, eyeWidth, eyeHeight);
+
+    // Pupils
+    stroke("black");
+    strokeWeight(1);
+    fill("black");
+    ellipse(170, 170, pupilWidth, pupilHeight);
+    ellipse(230, 170, pupilWidth, pupilHeight);
+
+    // Mouth
+    fill("pink");
+    arc(200, 230, 50, 50, 0, 180);
+
+    // Text
+    fill("black");
+    textSize(15);
+    text("We will customize this text later on in the project\nto reflect your avatar.", 25, 50);
 }
